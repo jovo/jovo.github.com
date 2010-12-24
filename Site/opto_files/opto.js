65dbf5e3afc82806f8dd2e4962e35f7302e58808
jovo <joshuav@jhu.edu>
2010-12-24 17:04:12 -0500

    total makeover using frank wood design

--- Site/opto_files/opto.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 2.0.4 local-build-20101017
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('opto_files/optoMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
--- Site/party_files/party.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 2.0.4 local-build-20101017
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('party_files/partyMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');performPostEffectsFixups()}
